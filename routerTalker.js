9de86dcb74e833061542d704968d6ed65b2ec79d
isabelanery <isabelanery242@gmail.com>
2022-06-17 16:28:17 -0300

    fix: try&catch problem

--- routerTalker.js
+++ routerTalker.js
@@ -1,23 +1,16 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs/promises'); // this fixed the try/catch problem
 
 const router = express.Router();
 
 const talker = './talker.json';
 router.get('/', async (_req, res, next) => {
-  // try {
-  //   const data = await fs.readFile(talker, 'utf8'); >>> this piece of code wasn't working, err: must have a callback, recieved utf8 instead. idk why tho, seems to work on the tutorias I saw
-  //   res.status(200).json(data);
-  // } catch (err) {
-  //   next(err);
-  // }
-  
-  const data = await fs.readFile(talker, 'utf8', (err, content) => {
-    if (err) return next(err);
-
-    res.status(200).json(JSON.parse(content));
-  });
-  return data;
+  try {
+    const data = JSON.parse(await fs.readFile(talker, 'utf8'));
+    return res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
