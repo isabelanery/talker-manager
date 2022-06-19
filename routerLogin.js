fad839a3fe631c71e5e618afb9fd7ccc3385c3ec
isabelanery <isabelanery242@gmail.com>
2022-06-19 18:51:56 -0300

    feat 4 approved

--- routerLogin.js
+++ routerLogin.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const crypto = require('crypto');
+const validator = require('validator');
 
 const router = express.Router();
 
 const loginList = [];
 
-router.post('/', async (req, res) => {
+const validateLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ 
+    message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({ 
+      message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+};
+
+router.post('/', validateLogin, (req, res) => {
   const { email, password } = req.body;
   loginList.push({ email, password });
   const token = crypto.randomBytes(8).toString('hex');
